dd818c949b300621b09c45c53837d21b9a8f0404
MunzurulAzam <munzurul1998@gmail.com>
2023-02-18 05:09:23 +0600

    update cont..

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end('My name is Munzurul Azam');
+    // 
+    if (req.url === '/about')
+        res.end('The about page')
+    else if (req.url === '/contact')
+        res.end('The contact page')
+    else if (req.url === '/')
+        res.end('The home page')
+    else {
+        req.writeHead(404)
+        res.end('page not found')
+    }
 });
 server.listen(4000);
 
